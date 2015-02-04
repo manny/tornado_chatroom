e4a1414255b879e72d0caa7c32569c94bf6884bb
Manuel Lopez <manny.lopez625@gmail.com>
2015-02-03 23:36:53 -0500

    got logging to finally work

--- chatroom.py
+++ chatroom.py
@@ -1,8 +1,12 @@
+import logging
 import tornado.ioloop
-import torando.web
+import tornado.web
+import tornado.websocket
+import tornado.options
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        logging.info("message served")
         self.write("too far away!")
 
 application = tornado.web.Application([
@@ -10,5 +14,6 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
+    tornado.options.parse_command_line()
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
