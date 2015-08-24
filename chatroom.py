f7f593aebe3889049a77114a91bb418cb86333a4
Manuel Lopez <manny@chartbeat.com>
2015-08-24 09:47:27 -0400

    testing remote connection

--- chatroom.py
+++ chatroom.py
@@ -11,18 +11,38 @@ from tornado.options import define, options, parse_command_line
 define ("port", default=8888, help="run on the given port", type=int)
 define ("debug", default=False, help="run in debug mode")
 
+#global_message_buffer = MessageBuffer()
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         logging.info("message served")
         self.render("index.html")
 
+class MessageNewHandler(tornado.web.RequestHandler):
+    def post(self): 
+        message = {
+            "id" : str(uuid.uuid4()),
+            "body" : self.get_argument("body"),
+        }
+        message["html"] = tornado.escapce.to_basestring(
+                self.render_string("message.html", message=message))
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next")) 
+        else:
+            self.write(message)
+        global_message_buffer.new_messages([message])
+     
+
+
+#class MessageUpdatesHandler(tornado.web.RequestHandler):
+    
 
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
             (r"/", MainHandler)
-            ],
+        ],
         cookie_secret="Figure this out later",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
